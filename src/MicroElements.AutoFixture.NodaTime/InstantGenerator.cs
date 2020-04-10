85d5c638a26cb982374d53b51773928b7d884130
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-04-10 14:46:50 +0200

    Update packages and add InstantGenerator

---
+++ src/MicroElements.AutoFixture.NodaTime/InstantGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoFixture.Kernel;
+using NodaTime;
+
+namespace MicroElements.AutoFixture.NodaTime
+{
+    public class InstantGenerator : ISpecimenBuilder
+    {
+        public object Create(object request, ISpecimenContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            if (!typeof(Instant).Equals(request))
+            {
+                return new NoSpecimen();
+            }
+
+            return Instant.FromDateTimeUtc(DateTime.Now.ToUniversalTime());
+        }
+    }
+}
