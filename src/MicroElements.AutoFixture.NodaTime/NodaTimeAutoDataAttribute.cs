c97eaa2f33b42a045e436dc9bb53bd875c269fb6
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2020-04-10 20:35:43 +0200

    Remove xunit dependency

--- src/MicroElements.AutoFixture.NodaTime/NodaTimeAutoDataAttribute.cs
+++ src/MicroElements.AutoFixture.NodaTime/NodaTimeAutoDataAttribute.cs
@@ -1,22 +1,22 @@
-﻿// Copyright (c) MicroElements. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using AutoFixture;
-using AutoFixture.Xunit2;
-
-namespace MicroElements.AutoFixture.NodaTime
-{
-    /// <summary>
-    /// Provides auto-generated data for NodaTime in xUnit.net's Theory attribute.
-    /// </summary>
-    public sealed class NodaTimeAutoDataAttribute : AutoDataAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="NodaTimeAutoDataAttribute"/> class.
-        /// </summary>
-        public NodaTimeAutoDataAttribute()
-            : base(() => new Fixture().Customize(new NodaTimeCustomization()))
-        {
-        }
-    }
-}
+﻿// // Copyright (c) MicroElements. All rights reserved.
+// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+//
+// using AutoFixture;
+// using AutoFixture.Xunit2;
+//
+// namespace MicroElements.AutoFixture.NodaTime
+// {
+//     /// <summary>
+//     /// Provides auto-generated data for NodaTime in xUnit.net's Theory attribute.
+//     /// </summary>
+//     public sealed class NodaTimeAutoDataAttribute : AutoDataAttribute
+//     {
+//         /// <summary>
+//         /// Initializes a new instance of the <see cref="NodaTimeAutoDataAttribute"/> class.
+//         /// </summary>
+//         public NodaTimeAutoDataAttribute()
+//             : base(() => new Fixture().Customize(new NodaTimeCustomization()))
+//         {
+//         }
+//     }
+// }
